76768f913eea56561d9373d2dcc502ad4b3bad7a
Adilson <lc.adilson@gmail.com>
2021-09-11 15:43:45 +1000

    Adding typeORM to the app's module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrganizationsModule } from './organizations/organizations.module';
+import typeOrmConfig from './config/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typeOrmConfig),
     OrganizationsModule,
   ],
   controllers: [AppController],
