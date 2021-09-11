b0ee8d974beef7afe0fd834e423327291a55dcc6
Adilson <lc.adilson@gmail.com>
2021-09-11 15:51:08 +1000

    Added the organization entity migration

---
+++ src/migrations/1631014641672-CreateOrganizationsTable.ts
@@ -0,0 +1,59 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateOrganizationsTable1631014641672
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'organizations',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v1mc()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: false,
+          },
+          {
+            name: 'slug',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamptz',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamptz',
+            isNullable: false,
+            default: 'now()',
+          },
+          { name: 'deleted_at', type: 'timestamptz', isNullable: true },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('organizations');
+  }
+}
