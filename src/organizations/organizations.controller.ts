8d4a0652e50957d3fe0bb47eda68c509522cf26d
Adilson <lc.adilson@gmail.com>
2021-09-12 21:47:02 +1000

    Added better UUID parsing for params
    It was firing an exception when an invalid value was given, making a
    500 Internal server error.
    
    QueryFailedError: invalid input syntax for type uuid: "1A"
    
    Now it delivers a better error message and a sweet 400 Bad request with
    a meaningfull response.
    
    {
        "error": "Bad Request",
        "message": "Validation failed (uuid  is expected)",
        "statusCode": 400
    }

--- src/organizations/organizations.controller.ts
+++ src/organizations/organizations.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { OrganizationsService } from './organizations.service';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
@@ -26,20 +27,20 @@ export class OrganizationsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.service.findOne(id);
   }
 
   @Patch(':id')
   update(
-    @Param('id') id: string,
+    @Param('id', ParseUUIDPipe) id: string,
     @Body() updateOrganizationDto: UpdateOrganizationDto,
   ) {
     return this.service.update(id, updateOrganizationDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.service.remove(id);
   }
 }
