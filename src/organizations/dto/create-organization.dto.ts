cfdef7b02f28bef6c4d036496ce4e4d2aa6f4920
Adilson <lc.adilson@gmail.com>
2021-09-12 22:30:15 +1000

    Added the validation decorators in our dto

--- src/organizations/dto/create-organization.dto.ts
+++ src/organizations/dto/create-organization.dto.ts
@@ -1,7 +1,17 @@
+import { IsIn, IsNotEmpty, IsString } from 'class-validator';
 import { OrganizationType } from '../entities/organization.type';
 
 export class CreateOrganizationDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly slug: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsIn(['condo', 'business', 'domain'])
   readonly type: OrganizationType;
 }
